4ca59a49b44eb0c7e9123e623b180a37c29cc020
teekaytech <taofeek.olalere@feghas.com>
2020-11-26 14:27:21 +0100

    Initial commit

---
+++ src/components/Dashboard.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Dashboard extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Dashboard</h2>
+        <h2>Dashboard</h2>
+      </div>
+    )
+  }
+}
+
+export default Dashboard
